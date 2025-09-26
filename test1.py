66df9b608602bf4aad083ae8850a7debd97165af
DinislamSuperYT <155798483+DinislamSuperYT@users.noreply.github.com>
2025-09-27 01:04:47 +0500

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,5 +1,16 @@
 import webbrowser
 
+def validator(func):
+    def wrapper(url):
+        if '.' in url: #если в тксте есть точка тогда выполнять
+            func(url) #функцию func с передачей url
+        else: #иначе
+            print("Неверный URL!") #вывести текст на экран
+    return wrapper #вернуть wrapper
+
+@validator
+    
+
 def open_url(url):
     webbrowser.open(url)
 
